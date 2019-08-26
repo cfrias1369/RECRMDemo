2ba528ab2e3fb0146ea7a084621e860728daf3de
cfrias <cfrias1369@yahoo.com>
2019-08-26 19:48:09 -0400

    Added Clients as a model, db context source, and mvc views (crud)

--- CoreWebApp/Data/RECRMDBContext.cs
+++ CoreWebApp/Data/RECRMDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using CoreWebApp.Models;
 
 namespace CoreWebApp.Models
 {
@@ -14,5 +15,7 @@ namespace CoreWebApp.Models
         }
 
         public DbSet<CoreWebApp.Models.Prospect> Prospects { get; set; }
+
+        public DbSet<CoreWebApp.Models.Client> Clients { get; set; }
     }
 }
