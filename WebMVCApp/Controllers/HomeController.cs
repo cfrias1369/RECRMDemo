8dbe6be444e8fb2bf7ac5cb80a0016cfa0ac32d5
cfrias <cfrias1369@yahoo.com>
2019-09-25 21:40:30 -0400

    Updated the About page (testing Azure Pipelines release) (2140Std)

--- WebMVCApp/Controllers/HomeController.cs
+++ WebMVCApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebMVCApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page. Test: 0925T2123";
+            ViewBag.Message = "Your application description page. Test: 0925T2140";
 
             return View();
         }
